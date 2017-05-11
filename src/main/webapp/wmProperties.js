c08e433e533f82ef65a045e624d95c93ff27555a
root <root@e5058a25df0e>
2017-05-11 09:04:06 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "AWSDeployment",
+  "homePage" : "Main",
+  "name" : "AWSDeployment",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
